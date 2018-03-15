8dc33bab7a609b965d40af896039f1f99731a2c2
aravind <ara@gmail.com>
2018-03-15 21:23:25 +0530

    this is for module 1 assignment

---
+++ app.js
@@ -0,0 +1,32 @@
+(function(){
+'use strict';
+
+angular.module("myFirstApp",[])
+.controller("myFirstController",function($scope){
+$scope.totvalue="";
+$scope.getData=function()
+{
+
+
+  var totvaluename=number_of_items($scope.data);
+  $scope.lenn=totvaluename;
+  if (totvaluename<=3 && totvaluename>0) {
+    $scope.totvalue="Enjoy!";
+  }
+  else if(totvaluename>3){
+    $scope.totvalue="Too much!";
+  }
+  else {
+    $scope.totvalue="Please enter data first";
+  }
+$scope.data="";
+};
+function number_of_items(string)
+{
+  var totvalue=string.split(",");
+
+  return totvalue.length;
+}
+});
+
+})();
